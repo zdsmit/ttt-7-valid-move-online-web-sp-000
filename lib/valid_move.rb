7f5146080576bc3a31f2aaf28960f6e7ade77161
Zach  Smith <zdsmith001@gmail.com>
2019-06-27 18:29:39 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,8 +8,9 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  index.between?(0,8)
-  if position_taken?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+    true
+  else
     false
   end
 end
